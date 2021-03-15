3a38073a84b2721621dc651a60e63dd18eca8d31
Nicole George <hello@nicolegeorge.me>
2021-03-15 16:32:53 +1030

    shifting todos state to top level - app.js

--- react-todo-list/src/components/Todos.js
+++ react-todo-list/src/components/Todos.js
@@ -1,33 +1,4 @@
-import { useState } from "react";
-
-const Todos = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "Make Hair Appointment",
-      day: "March 20 at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Code Blog-Shop Application",
-      day: "March 21 at 11:00am",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Buy Cffee",
-      day: "March 22 at 7:30am",
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: "Update Portfolio Website",
-      day: "March 23 at 4:30pm",
-      reminder: false,
-    },
-  ]);
-
+const Todos = ({ todos }) => {
   return (
     <>
       {todos.map((todo) => (
