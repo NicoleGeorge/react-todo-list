8627b3ff22875345b30c05cd651b4ce7a44426b7
Nicole George <hello@nicolegeorge.me>
2021-03-15 15:35:07 +1030

    adding header

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,10 +1,12 @@
+import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello from Nicole!</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
+
 
 export default App;
